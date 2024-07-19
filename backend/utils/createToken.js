dcebcc90ec21e8fa728b9243e187f27aa4d21368
chdas-programmer <141399154+chdas-programmer@users.noreply.github.com>
2024-07-19 14:05:45 +0530

    Update createToken.js

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -6,10 +6,10 @@ const generateToken = (res, userId) => {
   });
 
   // Set JWT as an HTTP-Only Cookie
-  res.cookie("jwt", token, {
+  res.cookie('jwt', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict",
+    secure: true, // Ensure HTTPS in production
+    sameSite: 'Lax',
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 
